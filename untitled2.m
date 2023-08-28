28c96ea44b476c622ce1f7338037018c79f6b4bf
Kathy <kaiyun@live.unc.edu>
2023-08-27 21:52:11 -0400

    Update figure codes

--- untitled2.m
+++
@@ -1,4 +0,0 @@
-for i = 2:30
-make_movie_gradually_drop_k3(sprintf('220410.6.4-2-P6a_1.5e-05-Cdc42_5000-Bem_500-P6b_0.011-lambda3_500-nY_1000-min_lambda3-95-%d.xyz',i),...
-    551,'/Users/kai/Downloads/pheromone-polarization/Kaiyun_WorkingDirectory/video/',sprintf('220410.6.4-2-P6a_1.5e-05-Cdc42_5000-Bem_500-P6b_0.011-lambda3_500-nY_1000-min_lambda3-95-%d',i))
-end
